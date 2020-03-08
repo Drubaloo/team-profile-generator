1d4167b4a0fe0c408b88c8dc54ccf89ef3a08513
Drubaloo <Drubaloo@outlook.com>
2020-03-07 17:40:15 -0800

    updated classes

--- app.js
+++ app.js
@@ -4,30 +4,118 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
+
+
 // Write code to use inquirer to gather information about the development team members,
+function manager() {
+    console.log(`Let's input your Manager!`)
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: `What is your Manager's name`,
+                name: `managerName`
+            },
+            {
+                type: "input",
+                message: `What is your Manager's ID number?`,
+                name: `managerId`
+            },
+            {
+                type: "input",
+                message: `What is your Manager's email?`,
+                name: `managerEmail`
+            },
+            {
+                type: "input",
+                message: `What is your Manager's office number?`,
+                name: `managerNumber`
+            }
+    ]).then((answers) => {
+            const currentManager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerNumber)
+        })
+}
+
+function engineer() {
+    console.log(`Let's input your Engineer!`)
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: `What is your Engineer's name`,
+                name: `engineerName`
+            },
+            {
+                type: "input",
+                message: `What is your Engineer's ID number?`,
+                name: `EngineerId`
+            },
+            {
+                type: "input",
+                message: `What is your Engineer's email?`,
+                name: `EngineerEmail`
+            },
+            {
+                type: "input",
+                message: `What is your Engineer's Github Username?`,
+                name: `engineerGithub`
+            }
+    ]).then((answers) => {
+            const currentEngineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
+        })
+}
+
+function engineer() {
+    console.log(`Let's input your Engineer!`)
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: `What is your Engineer's name`,
+                name: `engineerName`
+            },
+            {
+                type: "input",
+                message: `What is your Engineer's ID number?`,
+                name: `EngineerId`
+            },
+            {
+                type: "input",
+                message: `What is your Engineer's email?`,
+                name: `EngineerEmail`
+            },
+            {
+                type: "input",
+                message: `What is your Engineer's Github Username?`,
+                name: `engineerGithub`
+            }
+    ]).then((answers) => {
+            const currentEngineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
+        })
+}
+
+
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
